df0aa583e33b0f3b4064dfbbbfed85c568e00699
chanuka@codecommit-at-789734661575 <chanuka@blackswan-technologies.com>
2022-11-06 13:49:14 +0530

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ import 'pages/AboutPage.dart';
 import 'pages/AnimalPage.dart';
 
 import 'pages/ContactPage.dart';
-
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,78 +18,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class TabNavigationItem {
-  final Widget page;
-  final String title;
-  final Icon icon;
-  TabNavigationItem({
-    required this.page,
-    required this.title,
-    required this.icon,
-  });
-  static List<TabNavigationItem> get items => [
-        TabNavigationItem(
-          page: HomePage(),
-          icon: const Icon(Icons.home),
-          title: "Home",
-        ),
-        TabNavigationItem(
-          page: ContactPage(),
-          icon: const Icon(Icons.email),
-          title: "Contact",
-        ),
-        TabNavigationItem(
-          page: const AboutPage(),
-          icon: const Icon(Icons.info),
-          title: "About",
-        ),
-        TabNavigationItem(
-          page: AnimalPage(),
-          icon: const Icon(Icons.pets),
-          title: "Animals",
-        ),
-      ];
-}
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  static Route<dynamic> route() => MaterialPageRoute(
-        builder: (context) => const MyHomePage(),
-      );
+  MyHomePage({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: [
-          for (final tabItem in TabNavigationItem.items) tabItem.page,
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.red,
-        unselectedItemColor: Colors.black45,
-        onTap: (int index) => setState(() => _currentIndex = index),
-        items: [
-          for (final tabItem in TabNavigationItem.items)
-            BottomNavigationBarItem(
-              icon: tabItem.icon,
-              label: tabItem.title,
-            )
-        ],
+      body: Center(
+        child: Text(
+          'Your starting app',
+        ),
       ),
     );
   }
